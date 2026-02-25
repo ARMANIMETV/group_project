24961305d8ab462821fd9412ef1a7a7c5f514593
Massoud277-251 <massoudmahamatali2@gmail.com>
2026-02-25 15:54:05 +0400

    Add files via upload

--- variables.c
+++ variables.c
@@ -19,7 +19,7 @@ printf("My number is %d and my letter is %c\n", myNum, myLetter);
 printf("My favorite number is: %d", 21); // this will print a value without a variable
 printf("My favorite letter is: %c\n", 'A'); // this too will print a value without a variable
 
-int x = 5,y = 10;
+int x = 3,y = 6;
 printf("%d\n", x + y); //this will initiate Addition
 float z;
 z = 3.9;
